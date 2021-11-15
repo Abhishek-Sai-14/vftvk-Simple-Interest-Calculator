630587179037a53ebebd9cb40449fe4e5ee37f19
A.M. ABHISHEK SAI <83504755+Abhishek-Sai-14@users.noreply.github.com>
2021-11-15 12:57:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
-function compute()
-{
+function compute(){
+    //variables and some math from instructions
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
+    
+
+    document.getElementById("result").innerHTML=`If you deposit <span class="highlight">${principal}</span>,<br/>
+    at an interest rate of <span class="highlight">${rate}%</span>.<br/>
+    You will receive an amount of <span class="highlight">${interest}</span>,<br/>
+    in the year <span class="highlight">${year}</span><br /><br />`;
+    //also, changed innerText to innerHTML
+
 }
-function updateRate() 
-{
+
+//updates the html for Rates
+function updateRate(){
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText= `${rateval}%`;
 }
-document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
-        
-        
+
+//checks that Amount (principal) is not blank and is a positive number greater than 0
+function checkValue() {
+    //setting principal value to var amount
+    var amount = document.getElementById("principal").value;
+    //placing focus on principal input if any one condition is true
+    if((amount == "") || (amount == undefined) || (amount == NaN) || (amount <= 0)){
+        alert("Enter a positive number");
+        //resets values on the page when the principal is an invalid value
+        resetValues();
+        //sets focus on the principal input box
+        principal.focus();
+        return false;
+    }else{
+    //runs compute() if other conditions are false
+        return compute();
+    }
+}
+
+//funtion to reset values on the page
+function resetValues(){
+    document.getElementById("result").innerText = "";
+    document.getElementById("principal").value = "";
+    document.getElementById("rate").value = 10.25;
+    updateRate();
+    document.getElementById("years").selectedIndex = 0;
+}
